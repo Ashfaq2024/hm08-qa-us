72d6724dbec65883432694a1293696539102fab8
Ashfaq <Ashfaqrahmanadil@gmail.com>
2024-10-05 22:12:48 -0400

    Initial commit

--- page.js
+++ page.js
@@ -1,3 +1,87 @@
+class OrderPage {
+    // Selectors
+    get addressInput() { return $('#address-input'); }
+    get submitAddressButton() { return $('#submit-address'); }
+    get addressConfirmation() { return $('#address-confirmation'); }
+
+    get planDropdown() { return $('#support-plan-dropdown'); }
+    get supportivePlanOption() { return $('#support-plan-option-supportive'); }
+    get selectedPlan() { return $('#selected-support-plan'); }
+
+    get phoneInput() { return $('#phone-number'); }
+    get phoneConfirmButton() { return $('#phone-number-confirm'); }
+    get phoneConfirmation() { return $('#phone-confirmation'); }
+
+    get addCardButton() { return $('#add-card'); }
+    get cardNumberInput() { return $('#card-number'); }
+    get cardExpiryInput() { return $('#card-expiry'); }
+    get cardCVVInput() { return $('#card-cvv'); }
+    get linkCardButton() { return $('#link-card'); }
+    get cardLinkConfirmation() { return $('#card-link-confirmation'); }
+
+    get driverMessageInput() { return $('#driver-message'); }
+    get submitMessageButton() { return $('#submit-driver-message'); }
+    get messageConfirmation() { return $('#message-confirmation'); }
+
+    get blanketCheckbox() { return $('#blanket-checkbox'); }
+    get handkerchiefCheckbox() { return $('#handkerchief-checkbox'); }
+
+    get iceCreamDropdown() { return $('#ice-cream-dropdown'); }
+    get iceCreamOption2() { return $('#ice-cream-option-2'); }
+    get iceCreamOrderConfirmation() { return $('#ice-cream-order'); }
+
+    get orderButton() { return $('#order-taxi-button'); }
+    get carSearchModal() { return $('#car-search-modal'); }
+
+    get driverInfo() { return $('#driver-info'); }
+
+    // Actions
+    open() {
+        browser.url('/order-taxi');
+    }
+
+    setPickupAddress(address) {
+        this.addressInput.setValue(address);
+        this.submitAddressButton.click();
+    }
+
+    selectSupportivePlan() {
+        this.planDropdown.click();
+        this.supportivePlanOption.click();
+    }
+
+    setPhoneNumber(phone) {
+        this.phoneInput.setValue(phone);
+        this.phoneConfirmButton.click();
+    }
+
+    addCreditCard(number, expiry, cvv) {
+        this.addCardButton.click();
+        this.cardNumberInput.setValue(number);
+        this.cardExpiryInput.setValue(expiry);
+        this.cardCVVInput.setValue(cvv);
+        browser.keys('Tab');
+        this.linkCardButton.click();
+    }
+
+    addDriverMessage(message) {
+        this.driverMessageInput.setValue(message);
+        this.submitMessageButton.click();
+    }
+
+    orderBlanketAndHandkerchiefs() {
+        this.blanketCheckbox.click();
+        this.handkerchiefCheckbox.click();
+    }
+
+    orderIceCream() {
+        this.iceCreamDropdown.click();
+        this.iceCreamOption2.click();
+    }
+}
+
+module.exports = new OrderPage();
+
 module.exports = {
     // Inputs
     fromField: '#from',
--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -1,23 +1,65 @@
-const page = require('../../page');
-const helper = require('../../helper')
+const OrderPage = require('../../pages/OrderPage');
 
-describe('Create an order', () => {
-    it('should open phone number modal', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumberButton = await $(page.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const pnoneNumberModal = await $(page.phoneNumberModal);
-        await expect(pnoneNumberModal).toBeExisting();
-    })
+describe('Order Taxi Tests', () => {
 
-    it('should save the phone', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumber = helper.getPhoneNumber("+1");
-        await page.submitPhoneNumber(phoneNumber);
-        await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-    })
-})
+    // This will run before each test to open the page in a new session
+    beforeEach(() => {
+        browser.reloadSession();
+        OrderPage.open();
+    });
 
+    it('should allow the user to set a pickup address', () => {
+        OrderPage.setPickupAddress('123 Main St');
+        expect(OrderPage.addressConfirmation).toHaveTextContaining('123 Main St');
+    });
+
+    it('should allow the user to select a supportive plan', () => {
+        OrderPage.selectSupportivePlan();
+        expect(OrderPage.selectedPlan).toHaveTextContaining('Supportive');
+    });
+
+    it('should allow the user to fill in the phone number', () => {
+        OrderPage.setPhoneNumber('555-123-4567');
+        expect(OrderPage.phoneConfirmation).toHaveTextContaining('555-123-4567');
+    });
+
+    it('should allow the user to add a credit card', () => {
+        OrderPage.addCreditCard('4111 1111 1111 1111', '12/25', '123');
+        expect(OrderPage.cardLinkConfirmation).toHaveTextContaining('Card linked successfully');
+    });
+
+    it('should allow the user to write a message for the driver', () => {
+        OrderPage.addDriverMessage('Please come to the side entrance.');
+        expect(OrderPage.messageConfirmation).toHaveTextContaining('Please come to the side entrance.');
+    });
+
+    it('should allow the user to order a blanket and handkerchiefs', () => {
+        OrderPage.orderBlanketAndHandkerchiefs();
+        expect(OrderPage.blanketCheckbox).toBeSelected();
+        expect(OrderPage.handkerchiefCheckbox).toBeSelected();
+    });
+
+    it('should allow the user to order 2 ice creams', () => {
+        OrderPage.orderIceCream();
+        expect(OrderPage.iceCreamOrderConfirmation).toHaveTextContaining('2 Ice Creams');
+    });
+
+    it('should display the car search modal', () => {
+        OrderPage.orderButton.click();
+        expect(OrderPage.carSearchModal).toBeDisplayed();
+    });
+
+    it('should wait for the driver information to appear in the modal', () => {
+        expect(OrderPage.carSearchModal).toBeDisplayed();
+
+        browser.waitUntil(() => {
+            return OrderPage.driverInfo.isDisplayed();
+        }, {
+            timeout: 10000,
+            timeoutMsg: 'Driver information did not appear within 10 seconds'
+        });
+
+        expect(OrderPage.driverInfo).toHaveTextContaining('Driver: John Doe');
+    });
+
+});
